0b8317e8cbedb930a7a8695d6a1ad2a92ae414cd
Danilo Soares <danilo.silva@nutes.ufpe.br>
2023-04-27 11:01:56 -0300

    Continuando com JAVA

---
+++ SistemaBancarioSENAC/SistemaBancarioSENAC/src/Model/Endereco.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Endereco {
+	String uf;
+	String cidade;
+	String bairro;
+	String rua;
+	int numero;
+	String complemento;
+	String cep;
+
+}
---
+++ SistemaBancarioSENAC/SistemaBancarioSENAC/src/Principal/Principal.java
@@ -0,0 +1,15 @@
+package Principal;
+
+import Model.Endereco;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// System.out.println("Olá mundo!");
+		Endereco endAna; // Variável do tipo Endereco
+		endAna = new Endereco(); // Criando um objeto do tipo Endereco
+		System.out.println(endAna.toString());
+	}
+
+}
