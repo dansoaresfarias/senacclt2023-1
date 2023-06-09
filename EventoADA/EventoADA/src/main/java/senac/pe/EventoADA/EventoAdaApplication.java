91e3a69660cdd42143b3cf98e677ddad48fca04c
Danilo Soares <danilo.silva@nutes.ufpe.br>
2023-06-09 12:26:14 -0300

    Criando um novo projeto

---
+++ EventoADA/EventoADA/src/main/java/senac/pe/EventoADA/EventoAdaApplication.java
@@ -0,0 +1,13 @@
+package senac.pe.EventoADA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoAdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoAdaApplication.class, args);
+	}
+
+}
