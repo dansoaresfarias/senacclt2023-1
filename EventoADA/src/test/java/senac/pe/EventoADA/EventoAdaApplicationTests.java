149b7cad6239f9514d7c3e31642b97223818453f
Danilo Soares <danilo.silva@nutes.ufpe.br>
2023-06-09 12:29:22 -0300

    Inicializando o projeto Spring

---
+++ EventoADA/src/test/java/senac/pe/EventoADA/EventoAdaApplicationTests.java
@@ -0,0 +1,13 @@
+package senac.pe.EventoADA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoAdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
