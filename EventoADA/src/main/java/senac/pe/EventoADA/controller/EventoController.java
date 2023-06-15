e288fc53be81e84f3aa2cfa547e12e4a2db8528f
Danilo Soares <danilo.silva@nutes.ufpe.br>
2023-06-15 15:25:05 -0300

    Listar Eventos

--- EventoADA/src/main/java/senac/pe/EventoADA/controller/EventoController.java
+++ EventoADA/src/main/java/senac/pe/EventoADA/controller/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import senac.pe.EventoADA.Repository.EventoRepository;
 import senac.pe.EventoADA.model.Evento;
@@ -23,6 +24,14 @@ public class EventoController {
 	public String formCadstroEvento(Evento evento) {
 		er.save(evento);
 		return "redirect:/cadastrarEvento";
+	}
+	
+	@GetMapping("/listarEventos")
+	public ModelAndView listarEventos() {
+		ModelAndView mv = new ModelAndView("evento/listarEventos");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
 
 }
